bc7d90bfe41bc47525fef2bf4df4b3e12aed9cb1
akoolarni <arinpk210@gmail.com>
2023-12-14 22:58:30 +0530

    added fragments

---
+++ lib/donations_fragment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DonationsFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Your Donations Fragment'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,58 @@
 import 'package:flutter/material.dart';
+import 'feed_fragment.dart';
+import 'donations_fragment.dart';
+import 'profile_fragment.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: HomeActivity(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp();
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({Key? key}) : super(key: key);
+
+  @override
+  _HomeActivityState createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
+  int _currentIndex = 0;
+
+  final List<Widget> _fragments = [
+    FeedFragment(),
+    DonationsFragment(),
+    ProfileFragment(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Activity'),
+      ),
+      body: _fragments[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Donations',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
