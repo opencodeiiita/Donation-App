26ec850d47a2a22bfe9945a2d6a9ed7b09173494
Parth <revanwarparth@gmail.com>
2023-12-29 03:23:32 +0530

    cleanup donation card

--- lib/feedPage.dart
+++ lib/feedPage.dart
@@ -1,3 +1,4 @@
+import 'package:donation_app/Donation_card.dart';
 import 'package:flutter/material.dart';
 
 
@@ -87,96 +88,10 @@ class _feedPageState extends State<feedPage> {
                 ),
                 ListView.builder(
                   shrinkWrap: true,
-                  physics: ScrollPhysics(),
-                  itemCount: 5,
+                  physics: const ScrollPhysics(),
+                  itemCount: 3,
                   itemBuilder: (context,index){
-                    return Container(
-                      height: 260,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 250, 250, 255),
-                        borderRadius: BorderRadius.circular(30),
-                        border: Border.all(width: 1, color: Colors.lightBlueAccent),
-                      ),
-                      margin: EdgeInsets.fromLTRB(15, 7.5, 15, 7.5),
-                      child: Padding(
-                        padding:  EdgeInsets.all(20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Container(
-                              height: 100,
-                              padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(25),
-                                child: Image(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(
-                                    'assets/images/screen1.png',
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  flex: 2,
-                                  child: Container(
-                                    child: Text(
-                                      'Education Dono for poor child',
-                                      softWrap: true,
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(child: SizedBox(),flex:1,),
-                              ],
-                            ),
-                            Text(
-                              'By IIITA',
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.blueGrey
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      child: Text(
-                                        '\$ 4525 Raised ',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w500,
-                                          color: Color.fromARGB(255, 32, 159, 166),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Container(
-                                      child: Text(
-                                        'Target-\$10000',
-                                        textAlign: TextAlign.right,
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
+                    return const DonationCard();
                   }
                 ),
             ],
